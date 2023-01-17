6fb1ddbd99fd55ad4817e6a028aa29e9f65d84b6
Ronak <ronak.sheth@sheridancollege.ca>
2023-01-17 11:58:29 -0500

    Added author comment to the file

--- src/student1/ReverseWord.java
+++ src/student1/ReverseWord.java
@@ -2,7 +2,7 @@
 package student1;
 
 import java.util.Scanner;
-
+// Author Mustafa
 public class ReverseWord {
         public static void main(String[] args){
             /* char[] myWord = new char[7];
