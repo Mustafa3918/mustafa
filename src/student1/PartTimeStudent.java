d4a63d75205b3fc5a59d34d41f562c33fa9fd724
Mustafa <khan697@sheridancollege.ca>
2023-01-27 21:45:03 -0500

    new part time student class

---
+++ src/student1/PartTimeStudent.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student1;
+
+/**
+ *
+ * @author theca
+ */
+public class PartTimeStudent extends Student1  {
+    
+    public PartTimeStudent(String studentId, String studentName) {
+        super(studentId, studentName);
+    }
+    
+}
